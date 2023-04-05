6ed179ab7c28c2df1654123a218f94aaadf2f4f3
mia <125868397+miaaK@users.noreply.github.com>
2023-04-06 02:05:36 +0900

    ✨ Update login form with required attribute for form validation and add register link

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div className="login">
       <h1>Login</h1>
       <form>
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
+        <input type="text" placeholder="username" required />
+        <input type="password" placeholder="password" required />
         <button>Login</button>
+        <p>There's an error!</p>
+        <span>
+          Don't have an account? <Link to="/register">Register</Link>
+        </span>
       </form>
     </div>
   );
