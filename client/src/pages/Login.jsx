e415445ee47a6d26dc9c5576670b8865b8ded683
mia <125868397+miaaK@users.noreply.github.com>
2023-04-06 01:15:59 +0900

    ✨ Add login page layout and styling with SCSS

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
