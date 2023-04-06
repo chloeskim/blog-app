e5d3fb09c6dc398bbaf082bd0b9239c9d7154847
mia <125868397+miaaK@users.noreply.github.com>
2023-04-07 04:49:45 +0900

    ✨ Add footer layout and SCSS styling

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Logo from "../img/logo.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <img src={Logo} alt="logo" />
+      <span>Share your journey to spark change 🚀</span>
+    </footer>
+  );
 };
 
 export default Footer;
