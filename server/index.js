844a42a24ef91e9e5f98555bd0d62aeef3c8e1b8
mia <125868397+miaaK@users.noreply.github.com>
2023-04-17 03:22:02 +0900

    🔐 Add cookie-parser middleware for user authentication

--- server/index.js
+++ server/index.js
@@ -3,13 +3,17 @@ import express from "express";
 import authRoutes from "./routes/auth.js";
 import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 let port = process.env.SERVER_PORT;
 
 const app = express();
 
+// middleware
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
