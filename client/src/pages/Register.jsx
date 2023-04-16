c39c732bcada57c197b653d7a3e841f4fb851765
mia <125868397+miaaK@users.noreply.github.com>
2023-04-17 02:41:26 +0900

    ✨ Update: integrate user registration form with backend API

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,16 +1,60 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [err, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/auth/register", inputs);
+      navigate("/login");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+  // console.log(inputs);
+
   return (
     <div className="register">
       <h1>Register</h1>
       <form>
-        <input type="text" placeholder="username" required />
-        <input type="email" placeholder="email" required />
-        <input type="password" placeholder="password" required />
-        <button>Login</button>
-        <p>There's an error!</p>
+        <input
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="email"
+          placeholder="email"
+          name="email"
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+          required
+        />
+        <button onClick={handleSubmit}>Register</button>
+        {err && <p>{err}</p>}
         <span>
           Do you have an account? <Link to="/login">Login</Link>
         </span>
