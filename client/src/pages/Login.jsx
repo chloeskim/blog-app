d84d3e93b1f55486ba54b67fcc1a8137ad89962a
mia <125868397+miaaK@users.noreply.github.com>
2023-04-17 03:26:02 +0900

    ✨ Update: integrate user login form with backend API

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,17 +1,53 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [err, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/auth/login", inputs);
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+
   return (
     <div className="login">
       <h1>Login</h1>
       <form>
-        <input type="text" placeholder="username" required />
-        <input type="password" placeholder="password" required />
-        <button>Login</button>
-        <p>There's an error!</p>
+        <input
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+          required
+        />
+        <button onClick={handleSubmit}>LOGIN</button>
+        {err && <p>{err}</p>}
         <span>
-          Don't have an account? <Link to="/register">Register</Link>
+          Don't have an account? <Link to="/register">Sign Up</Link>
         </span>
       </form>
     </div>
