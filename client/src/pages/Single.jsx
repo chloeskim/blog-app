6bb9cb75e2de9afe9d41fee39bed667a7d6505f0
mia <125868397+miaaK@users.noreply.github.com>
2023-04-20 04:57:31 +0900

    ✨Fetch single post by ID
    - Add useState hook to manage single post data
    - Get post ID from URL using useLocation hook
    - Use useEffect hook to fetch single post data from server based on ID

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,68 +1,54 @@
 // Single Post
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import axios from "axios";
+import moment from "moment";
 import Edit from "../img/icons/edit.png";
 import Delete from "../img/icons/delete.png";
 import Menu from "../components/Menu";
+import { AuthContext } from "../context/authContext";
 
 const Single = () => {
+  const [post, setPost] = useState([]);
+
+  const location = useLocation();
+
+  const postId = location.pathname.split("/")[2];
+
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/${postId}`);
+        setPost(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [postId]);
   return (
     <div className="single">
       <div className="content">
-        <img src="https://picsum.photos/id/182/600/500" alt="" />
+        <img src={post?.img} alt="" />
         <div className="user">
           <img src="https://picsum.photos/id/65/200/300" alt="" />
           <div className="info">
-            <span>Ashley</span>
-            <p>Posted 2 days ago</p>
+            <span>{post.username}</span>
+            <p>Posted {moment(post.date).fromNow()}</p>
           </div>
-          <div className="edit">
-            <Link to={`/write?edit=2`}>
-              <img src={Edit} alt="" />
-            </Link>
-            <img src={Delete} alt="" />
-          </div>
+          {currentUser.username === post.username && (
+            <div className="edit">
+              <Link to={`/write?edit=2`}>
+                <img src={Edit} alt="" />
+              </Link>
+              <img src={Delete} alt="" />
+            </div>
+          )}
         </div>
-        <h1>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis, atque.
-        </h1>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim
-          consequatur fugit, sit nam ipsum mollitia quod ex, veritatis minus
-          libero quis perferendis ratione error alias aperiam tenetur! Dicta,
-          quam commodi!
-          <br />
-          <br />
-          Ab porro harum, tenetur doloremque at quae itaque illum eaque eum
-          accusantium culpa laboriosam expedita nisi recusandae mollitia
-          praesentium facere explicabo adipisci officiis repellendus nobis,
-          natus eius? Deleniti, error facere! Optio veniam ipsam minus dolorem
-          maiores quia amet expedita commodi recusandae sapiente corporis quae
-          sequi, eaque cumque rem ducimus fugiat omnis voluptatum voluptate
-          nostrum culpa placeat architecto quos! Ipsam, voluptatibus? Id
-          molestiae distinctio neque, sunt iure nemo ad, blanditiis tenetur
-          quisquam saepe, alias dolorem aperiam reprehenderit.
-          <br />
-          <br />
-          At numquam quisquam voluptate esse ut atque dolorem aliquam, rerum
-          soluta ipsam. Voluptas, laudantium. Saepe enim tempora, ratione
-          quisquam quas distinctio cupiditate quidem nemo nobis, odit laudantium
-          cumque voluptas sint? Cum, praesentium! Blanditiis nihil recusandae
-          quos dignissimos at vel iste et ipsum natus odio. Quidem sint
-          accusantium at quod aliquam fugiat ad quam quasi, cupiditate
-          voluptatem, alias aut obcaecati, tenetur sapiente a impedit vitae nam
-          ea ratione porro. Numquam maxime accusamus veniam quo natus? Illum ex
-          veniam quasi soluta laborum ut itaque id iste praesentium repudiandae
-          harum provident perspiciatis beatae nobis, alias ea ad vel ullam natus
-          magnam odio, sunt modi necessitatibus? Incidunt, laudantium. Suscipit
-          dolor magnam mollitia, eos vel aliquam ipsam! Cumque dolore nam
-          maiores fuga voluptate. Sed, odit tempore! Dignissimos officiis,
-          perspiciatis, accusantium incidunt asperiores, dolorem repellendus
-          quas assumenda dolor nemo ut? Tempora blanditiis natus architecto
-          quibusdam quisquam? Deserunt sit praesentium, saepe modi quos labore
-          non harum adipisci eos distinctio tempora minima, amet neque nihil
-          animi quaerat accusamus, aliquam ut dolore suscipit.
-        </p>
+        <h1>{post.title}</h1>
+        <p>{post.desc}</p>
       </div>
       <Menu />
     </div>
